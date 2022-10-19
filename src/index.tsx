9907d1683439fb71993329877ff6bf90472b3cac
German <72597058+GM-GitH@users.noreply.github.com>
2022-10-19 21:47:38 +0200

    Change extensions JS to TSX

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+const container: HTMLElement = document.getElementById("root")!;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
